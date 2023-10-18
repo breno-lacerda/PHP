bfcc9e3f8f4d1095263840d25beefb65a4668397
Breno Lacerda <145488996+breno-lacerda@users.noreply.github.com>
2023-10-18 14:23:36 -0300

    Add files via upload

---
+++ aula01/exp01.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>Teste de modificação</h1>
+<?php
+$color = "red";
+echo "My car is " . $color . "<br>";
+echo "My house is " . $COLOR . "<br>";
+echo "My boat is " . $coLOR . "<br>";
+?> 
+</body>
+</html>
---
+++ aula01/exp05.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $curso = "Web Médio - Anulo 01!";
+    $disciplina = "PSW01";
+    $turma = 2022;
+    echo "[Curso] : $curso<br>";
+    echo "[Disciplina] : $disciplina<br>";
+    echo "[turma] : $turma";
+    ?>
+</body>
+</html>
