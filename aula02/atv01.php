6827bae86c038148a22cf0b9a404611ea5f9b6d3
Breno Lacerda <145488996+breno-lacerda@users.noreply.github.com>
2023-10-19 22:04:21 -0300

    Add files via upload

---
+++ aula02/atv01.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="atv01.css">
+</head>
+<body>
+
+
+    <div class="estilo1">
+       <div class="estilo4">
+           <h1>Black</h1>
+       </div>
+    </div>
+
+
+    <div class="estilo2">
+
+    </div>
+
+
+    <div class="estilo3">
+
+    </div>
+
+
+</body>
+</html>
