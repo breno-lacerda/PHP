d9feac1836d6272ed0d52b7d3fa1dafbdd49b024
Breno Lacerda <145488996+breno-lacerda@users.noreply.github.com>
2023-10-19 21:44:57 -0300

    aula02

---
+++ exp01.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exp01</title>
+    <link rel="stylesheet" href="exp01.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Pixelify+Sans&display=swap" rel="stylesheet">
+</head>
+<body>
+   <pre>Seletores</pre>
+   <h1>Título</h1>
+   <h2>Subtítulo</h2>
+   <p id="stilo1">parágrafo</p>
+   <pre id="stilo1"> seletores com id para especificar um elemento</pre>
+   <p>parágrafo</p>
+   <p>parágrafo</p>
+   <pre class="stilo2">seletores com classe para vários elementos</pre>
+   <p class="stilo2">usa para vários elementos</p>
+
+   
+
+</body>
+</html>
---
+++ exp02.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="exp02.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Pixelify+Sans&display=swap" rel="stylesheet">
+</head>
+<body>
+    <h1>Título</h1>
+    <p class="stilo"> classe do parágrafo</p>
+
+    <div class="conteudo">
+        <p id="Título da div">Título da div</p>
+           <div>
+              <p>subdivisão</p>
+              <p></p>
+           </div>
+    </div>
+</body>
+</html>
